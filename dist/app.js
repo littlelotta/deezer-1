49a41bd9a8effa571b458b9f3a8baa40ee74e7f1
nicco <nicco.borgioli@gmail.com>
2018-04-23 21:55:54 +0200

    Getting closer

---
+++ dist/app.js
@@ -0,0 +1,125 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Request = require("request");
+const md5_1 = require("./md5");
+const aesjs = require("./aes");
+const request = Request.defaults({
+    jar: Request.jar(),
+    headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' }
+});
+var FILE_TYPES;
+(function (FILE_TYPES) {
+    FILE_TYPES[FILE_TYPES["MP3_256"] = 1] = "MP3_256";
+    FILE_TYPES[FILE_TYPES["MP3_320"] = 3] = "MP3_320";
+    FILE_TYPES[FILE_TYPES["FLAC"] = 9] = "FLAC";
+})(FILE_TYPES || (FILE_TYPES = {}));
+class AuthObject {
+    constructor(token) {
+        this.token = token;
+    }
+    static extractTokenFromHtml(html) {
+        const tkn = this.rexp.exec(html);
+        if (tkn !== null)
+            return tkn[0].slice(13, -1);
+        return '';
+    }
+    static async getNewAuth() {
+        await new Promise(resolve => {
+            request({
+                url: 'https://www.deezer.com/ajax/action.php',
+                method: 'post',
+                form: {
+                    type: 'login',
+                    mail: 'tibegestep@hu4ht.com',
+                    password: 'tibegestep@hu4ht.com',
+                }
+            }, (err, res, body) => {
+                if (body !== 'success')
+                    throw new Error('Could not log in');
+                resolve();
+            });
+        });
+        const tknBody = await new Promise(resolve => {
+            request({
+                url: 'https://www.deezer.com/en/',
+                method: 'get'
+            }, (err, httpsResponse, body) => {
+                resolve(body);
+            });
+        });
+        return new this(this.extractTokenFromHtml(tknBody));
+    }
+}
+AuthObject.rexp = /checkForm ?= ?(\"|\').{32}\1/g;
+class DZCrypt {
+    static bfGenKey2(h1, h2) {
+        var l = h1.length, s = [];
+        for (var i = 0; i < l; i++)
+            s.push(this.bfGK.charCodeAt(i) ^ h1.charCodeAt(i) ^ h2.charCodeAt(i));
+        return s;
+    }
+    static bfGenKey(id, format) {
+        var h = md5_1.hex_md5(id + '');
+        var h1 = h.substr(0, 16), h2 = h.substr(16, 16);
+        var k = this.bfGenKey2(h1, h2);
+        if (!format)
+            return k;
+        return k.map(format == 'hex' ? ((a) => (a + 256).toString(16).substr(-2)) : ((a) => String.fromCharCode(a))).join('');
+    }
+    static zeroPad(b) {
+        const aesBS = 16;
+        const l = b.length;
+        if (l % aesBS != 0) {
+            if (typeof (b) === 'string')
+                b += '\0'.repeat(aesBS - (l % aesBS));
+            else
+                b = b.concat(Array.apply(null, Array(aesBS - (l % aesBS))).map(() => 0));
+        }
+        return b;
+    }
+    static encryptURL(track, fmt) {
+        const urlsep = '\xa4';
+        var str = [track.MD5_ORIGIN, fmt, track.SNG_ID, track.MEDIA_VERSION].join(urlsep);
+        str = DZCrypt.zeroPad([md5_1.hex_md5(str), str, ''].join(urlsep));
+        return aesjs.util.convertBytesToString((DZCrypt.urlCryptor.encrypt(aesjs.util.convertBytesToString(str, 'hex'))), 'hex');
+    }
+    static dzDownload(track, fmt = FILE_TYPES.MP3_320) {
+        var msg = [
+            'https://e-cdns-proxy-' + track.MD5_ORIGIN.charAt(0) + '.dzcdn.net' + '/mobile/1/' + this.encryptURL(track, fmt),
+            this.bfGenKey(track.SNG_ID, '')
+        ];
+        console.log(msg);
+    }
+}
+DZCrypt.bfGK = 'g4el58wc0zvf9na1';
+DZCrypt.urlCryptor = new aesjs.ModeOfOperation.ecb(aesjs.util.convertStringToBytes('jo6aey6haid2Teih'));
+class DZApi {
+    constructor(auth) {
+        this.auth = auth;
+    }
+    getTrackJSON(id) {
+        return new Promise((resolve, reject) => {
+            request({
+                url: `https://www.deezer.com/ajax/gw-light.php?method=deezer.pageTrack&input=3&api_version=1.0&api_token=${this.auth.token}`,
+                method: 'POST',
+                json: { "sng_id": id, "lang": "en", "tab": 0 }
+            }, (err, res, body) => {
+                if (Object.keys(body.results).length === 0)
+                    reject('Bad request');
+                resolve(body.results.DATA);
+            });
+        });
+    }
+}
+async function entry() {
+    try {
+        const auth = await AuthObject.getNewAuth();
+        const api = new DZApi(auth);
+        const info = await api.getTrackJSON(367505291);
+        DZCrypt.dzDownload(info);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+entry();
--- src/app.ts
+++ src/app.ts
@@ -1,78 +1,144 @@
 import * as https from 'https'
+import * as Request from 'request'
+import { IncomingMessage } from 'http'
+import { writeFileSync } from 'fs'
+import { hex_md5 } from './md5'
+import * as aesjs from './aes'
+import { reject } from 'bluebird';
 
-interface Options {
-	hostname: string
-	method: string
-	port?: number
-	path?: string
-	body?: any
+const request = Request.defaults({
+	jar: Request.jar(),
+	headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36' }
+})
+
+enum FILE_TYPES {
+	MP3_256 = 1,
+	MP3_320 = 3,
+	FLAC = 9,
+}
+
+class AuthObject {
+	static rexp = /checkForm ?= ?(\"|\').{32}\1/g
+
+	// constructor(public token: string, public sid: string) { }
+	constructor(public token: string) { }
+
+	private static extractTokenFromHtml(html: string): string {
+		const tkn = this.rexp.exec(html)
+		if (tkn !== null)
+			return tkn[0].slice(13, -1)
+		return ''
+	}
+
+	static async getNewAuth(): Promise<AuthObject> {
+
+		await new Promise(resolve => {
+			request({
+				url: 'https://www.deezer.com/ajax/action.php',
+				method: 'post',
+				form: {
+					type: 'login',
+					mail: 'tibegestep@hu4ht.com',
+					password: 'tibegestep@hu4ht.com',
+				}
+			}, (err, res, body) => {
+				if (body !== 'success') throw new Error('Could not log in')
+				resolve()
+			})
+		})
+
+		const tknBody: string = await new Promise<string>(resolve => {
+			request({
+				url: 'https://www.deezer.com/en/',
+				method: 'get'
+			}, (err, httpsResponse, body) => {
+				resolve(body)
+			})
+		})
+
+		return new this(this.extractTokenFromHtml(tknBody))
+	}
+}
+
+class DZCrypt {
+
+	private static bfGK = 'g4el58wc0zvf9na1'
+	private static urlCryptor = new aesjs.ModeOfOperation.ecb(aesjs.util.convertStringToBytes('jo6aey6haid2Teih'))
+
+	private static bfGenKey2(h1: string, h2: string): number[] {
+		var l = h1.length,
+			s = []
+		for (var i = 0; i < l; i++) s.push(this.bfGK.charCodeAt(i) ^ h1.charCodeAt(i) ^ h2.charCodeAt(i))
+		return s
+	}
+
+	private static bfGenKey(id: string, format: string): number[] | string {
+		var h = hex_md5(id + '')
+		var h1 = h.substr(0, 16),
+			h2 = h.substr(16, 16)
+		var k = this.bfGenKey2(h1, h2)
+		if (!format) return k
+		return k.map(format == 'hex' ? ((a: number) => (a + 256).toString(16).substr(-2)) : ((a: number) => String.fromCharCode(a))).join('')
+	}
+
+	private static zeroPad(b) {
+		const aesBS = 16
+		const l = b.length
+		if (l % aesBS != 0) {
+			if (typeof (b) === 'string') b += '\0'.repeat(aesBS - (l % aesBS))
+			else b = b.concat(Array.apply(null, Array(aesBS - (l % aesBS))).map(() => 0))
+		}
+		return b
+	}
+
+	private static encryptURL(track, fmt: FILE_TYPES) {
+		const urlsep = '\xa4'
+		var str = [track.MD5_ORIGIN, fmt, track.SNG_ID, track.MEDIA_VERSION].join(urlsep)
+		str = DZCrypt.zeroPad([hex_md5(str), str, ''].join(urlsep))
+		return aesjs.util.convertBytesToString(
+			(DZCrypt.urlCryptor.encrypt(
+				aesjs.util.convertBytesToString(str, 'hex')
+			)), 'hex')
+	}
+
+	static dzDownload(track, fmt = FILE_TYPES.MP3_320) {
+		var msg = [
+			'https://e-cdns-proxy-' + track.MD5_ORIGIN.charAt(0) + '.dzcdn.net' + '/mobile/1/' + this.encryptURL(track, fmt),
+			this.bfGenKey(track.SNG_ID, '')
+		]
+		console.log(msg)
+		// mainWk.postMessage(msg)
+	}
 }
 
 class DZApi {
 
-	static rexp = /checkForm ?= ?(\"|\')(\w|\.|~|-)*?\1;/g
+	constructor(public auth: AuthObject) { }
 
-	token: string = ''
-
-	private static request(options: Options): Promise<string> {
-		return new Promise(resolve => {
-
-			const ret: Buffer[] = [];
-
-			https.request(options, (res) => {
-
-				res.on('data',
-					data => Buffer.isBuffer(data) ? ret.push(data) : ret.push(new Buffer(data)))
-
-				res.on('end', () => resolve(Buffer.concat(ret).toString()))
-
-			}).on('error', (e) => {
-				resolve('')
-			}).end()
-		})
-	}
-
-	private extractTokenFromHtml(html: string): string {
-		const tkn = DZApi.rexp.exec(html)
-		if (tkn !== null)
-			return tkn[0].slice(13, -2)
-		return ''
-	}
-
-	getTrack(id: string | number): Promise<any | undefined> {
-		return new Promise(resolve => {
-			console.log(`/ajax/gw-light.php?method=deezer.pageTrack&input=3&api_version=1.0&api_token=${this.token}`)
-			DZApi.request({
-				hostname: 'www.deezer.com',
+	getTrackJSON(id: string | number): Promise<JSON> {
+		return new Promise((resolve, reject) => {
+			request({
+				url: `https://www.deezer.com/ajax/gw-light.php?method=deezer.pageTrack&input=3&api_version=1.0&api_token=${this.auth.token}`,
 				method: 'POST',
-				path: `/ajax/gw-light.php?method=deezer.pageTrack&input=3&api_version=1.0&api_token=${this.token}`,
-				body: { "sng_id": id, "lang": "en", "tab": 0 },
-			}).then(track => resolve(track))
-		})
-	}
-
-	getToken(): Promise<boolean> {
-		return new Promise(resolve => {
-			DZApi.request({
-				hostname: 'www.deezer.com',
-				port: 443,
-				path: '/en/',
-				method: 'GET'
-			}).then(html => {
-				this.token = this.extractTokenFromHtml(html)
-				resolve(true)
+				json: { "sng_id": id, "lang": "en", "tab": 0 }
+			}, (err, res, body) => {
+				if (Object.keys(body.results).length === 0) reject('Bad request')
+				resolve(body.results.DATA)
 			})
 		})
 	}
 }
 
 async function entry() {
-	const api = new DZApi()
-	await api.getToken()
+	try {
+		const auth = await AuthObject.getNewAuth()
+		const api = new DZApi(auth)
 
-	console.log(await api.getTrack(367505291))
-
-	console.log(api.token)
+		const info = await api.getTrackJSON(367505291)
+		DZCrypt.dzDownload(info)
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 entry()
