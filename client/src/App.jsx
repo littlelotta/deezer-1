070eb58dc9e611a9e776041867e5f3575b7dc61d
nicco <nicco.borgioli@gmail.com>
2018-04-30 15:53:05 +0200

    Cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 
 import Tabs from './containers/Tabs'
 
-export const URL = 'http://localhost:3000'
-
 export default () => (
-	<div className="container">
-		<div className="columns is-mobile">
-			<div className="column is-hidden-mobile" />
-			<div className="column is-12-mobile is-10-tablet is-9-desktop is-8-widescreen is-7-fullhd">
-				<Tabs />
-			</div>
-			<div className="column is-hidden-mobile" />
-		</div>
-	</div>
+	<Tabs />
 )
