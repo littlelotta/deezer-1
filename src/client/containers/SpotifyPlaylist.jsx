18da9053145a60875fbc947a4ddccca3c0a6b218
nicco <nicco.borgioli@gmail.com>
2018-05-02 22:27:44 +0200

    playlist page

--- src/client/containers/SpotifyPlaylist.jsx
+++ src/client/containers/SpotifyPlaylist.jsx
@@ -2,23 +2,61 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { ipcRenderer } from 'electron'
 
+import { getImageFromPlaylist } from './Spotify'
+
+export const separator = 'playlist:'
 class Spotify extends Component {
 
 	constructor(props) {
 		super(props)
 
-		this.state = {}
+		this.state = {
+			tracks: { items: [] }
+		}
+
+		this.getPlaylistLink = this.getPlaylistLink.bind(this)
+		this.getPlaylist = this.getPlaylist.bind(this)
+		this.getTracks = this.getTracks.bind(this)
+	}
+
+	componentDidMount() {
+		this.getPlaylist()
+	}
+
+	getPlaylistLink() {
+		return this.props.activeTab.slice((separator.length))
 	}
 
 	getPlaylist(id) {
+		ipcRenderer.send('Spotify', { action: 'get:playlist', payload: { link: this.getPlaylistLink() } })
+		ipcRenderer.once('Spotify', (event, arg) => {
+			console.log(arg)
+			this.setState(arg)
+			this.getTracks()
+		})
+	}
 
+	getTracks() {
+		ipcRenderer.send('Spotify', { action: 'get:iterable', payload: { iterable: this.state.tracks } })
+		ipcRenderer.once('Spotify', (event, arg) => {
+			this.setState({ tracks: Object.assign(this.state.tracks, { items: arg }) })
+		})
 	}
 
 	render() {
-		if (!this.props.activeTab.startsWith('playlist:')) return null
-		else return (
-			<div className="section">
-
+		return (
+			<div id="playlist" className="section no-padding-top">
+				<div className="box">
+					<figure className="image is-128x128">
+						<img src={getImageFromPlaylist(this.state)} />
+					</figure>
+					<span className="is-size-2">{this.state.name}</span>
+					<ul className="songs">
+						{this.state.tracks.items.map((track, i) => (
+							<li key={i + track.track.uri}>{track.track.name}</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		)
 	}
