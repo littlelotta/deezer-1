691297773ef0597abd400a31c9f22e53a279ea08
nicco <nicco.borgioli@gmail.com>
2018-05-04 18:02:55 +0200

    Added copy paste

--- src/electron/Menu.ts
+++ src/electron/Menu.ts
@@ -18,17 +18,11 @@ module.exports = [{
 		click: app.quit
 	}]
 }, {
-	label: 'Options',
-	submenu: [{
-		label: 'Select download folder',
-		click: setNewDlFolder
-	}, {
-		label: 'Reset Settings',
-		click: resetApp
-	}, {
-		label: 'Open Settings',
-		click: () => {
-			shell.openItem(dirname(Settings.getFileName()))
-		}
-	}]
+	label: "Edit",
+	submenu: [
+		{ label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+		{ label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+		{ label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+		{ label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+	]
 }]
